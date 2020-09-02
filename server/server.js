bba5e538bd3b337fcab713c86b91b01d5b0ce0ce
Jose Enrique <joseboba@pop-os.localdomain>
2020-09-02 15:56:08 -0600

    Fixed JSON error

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
-app.use(require('./routes/usuario.js'))
+app.use(require('./routes/index'))
 
 mongoose.connect(process.env.URLDB, 
     {   useNewUrlParser: true, 
