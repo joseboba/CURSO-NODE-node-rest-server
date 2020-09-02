4cb5b5d6b2d9c01f37019b533dfde4724c453062
Jose Enrique <joseboba@pop-os.localdomain>
2020-09-02 14:58:25 -0600

    Login Normal

--- server/config/config.js
+++ server/config/config.js
@@ -1,22 +1,38 @@
-// ===================
-//      Puerto
-// ===================
-process.env.PORT = process.env.PORT || 3000; 
+// ============================
+//  Puerto
+// ============================
+process.env.PORT = process.env.PORT || 3000;
 
 
-// ===================
-//      Entorno
-// ===================
-
+// ============================
+//  Entorno
+// ============================
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
-// =====================
-//      BAse de Datos
-// =====================
-let urlDB;
-if(process.env.NODE_ENV === 'dev') 
-    urlDB = 'mongodb://localhost:27017/cafe' 
-else
-    urlDB = process.env.MONGO_URI;
 
-process.env.URLDB = urlDB
+// ============================
+//  Vencimiento del Token
+// ============================
+// 60 segundos
+// 60 minutos
+// 24 horas
+// 30 días
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+
+// ============================
+//  SEED de autenticación
+// ============================
+process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
+
+// ============================
+//  Base de datos
+// ============================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = process.env.MONGO_URI;
+}
+process.env.URLDB = urlDB;
